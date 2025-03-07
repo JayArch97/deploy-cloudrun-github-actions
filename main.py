f5eeb482fb561d0b39ff497dd09a92699ebfcfb4
Julio Armando Quintanilla Acosta <121356473+JayArch97@users.noreply.github.com>
2025-03-06 18:14:05 -0600

    Update main.py

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ def get_time(request):
   request_json = request.get_json(silent=True)
   request_args = request.args
 
-  return (f"I am testing with the time at {timestamp}!")
+  return (f" Making another change {timestamp}!")
